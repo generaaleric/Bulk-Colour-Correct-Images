5b75565a20756b3568e40036a41428a743400a62
eric korevaar <ekorevaar01@hotmail.nl>
2024-02-05 10:32:50 +0100

    Add project files.

---
+++ Bulk Colour Correct Images.cpp
@@ -0,0 +1,17 @@
+// Bulk Colour Correct Images.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+#include "FunctionsHeader.h"
+
+int main()
+{
+    //initialise some of our needed variables
+    std::string NetworkAddress;
+
+    std::cout << "please input the full url where the images are: ";
+    std::cin >> NetworkAddress;
+
+    std::cout << NetworkAddress;
+}
